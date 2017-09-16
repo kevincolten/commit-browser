e19751a9b974c1fa90b8926017ef1f969c819542
Kevin Colten <kevincolten@gmail.com>
2017-09-15 22:31:29 -0500

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+    ignored: /node_modules/
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      }
+    ]
+  }
+}
